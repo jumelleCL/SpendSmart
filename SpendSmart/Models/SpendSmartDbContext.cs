6fe70b6c9582bfb6f3984b99627eedd675eb4c40
Camilo <jumellecamilo@gmail.com>
2024-08-02 04:11:30 +0200

    Added basic stuff for asp

---
+++ SpendSmart/Models/SpendSmartDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
